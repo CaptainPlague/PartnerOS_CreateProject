4c8fa79220e0ce6be237448aa2a35b9bd0d80669
CaptainPlague <marksouvannasouck@yahoo.com>
2023-04-04 20:29:01 +0200

    Use supabase cli

--- supabase.py
+++ supabase.py
@@ -1,6 +1,8 @@
-import subprocess
 import os
 import sys
+import subprocess
+from supabase import create_client
+from dotenv import load_dotenv
 
 
 def run_command(command):
@@ -30,6 +32,37 @@ def create_supabase_project(project_name):
     print(f"Supabase project created successfully: {project_name}")
 
 
+def get_supabase_client():
+    # Load environment variables from the .env file
+    load_dotenv()
+
+    # Get the Supabase API URL and key from environment variables
+    supabase_url = os.getenv("SUPABASE_URL")
+    supabase_key = os.getenv("SUPABASE_KEY")
+
+    # Create a Supabase client
+    supabase = create_client(supabase_url, supabase_key)
+
+    return supabase
+
+
+def fetch_data_from_table(table_name):
+    supabase = get_supabase_client()
+
+    # Fetch all rows from the specified table
+    response = supabase.from_(table_name).select()
+
+    if response.error:
+        print(f"Error fetching data from {table_name}: {response.error}")
+    else:
+        print(f"Data from {table_name}:")
+        for row in response.data:
+            print(row)
+
+
 if __name__ == "__main__":
     project_name = "my_supabase_project"
     create_supabase_project(project_name)
+
+    # Example usage: fetch data from a table called "users"
+    fetch_data_from_table("users")
